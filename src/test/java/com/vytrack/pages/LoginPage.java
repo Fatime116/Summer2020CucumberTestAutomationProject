93614fe6161389ac64afda22662c73d70b67a900
Fatime116 <patigul116@gmail.com>
2021-01-14 20:20:14 -0800

    added login page classes @FindBy and login methods

---
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -0,0 +1,71 @@
+package com.vytrack.pages;
+
+import com.vytrack.utils.ConfigurationReader;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+     //Constructor cannot be inheritted, but child class can call parent class constructor
+    //when we create object from this class, jvm will add runtime constructor, and this constructor can
+    //call basepage constructor, so initElements method will gets executed once it extends to it and create object
+
+
+
+    //To not use web elements directly in step definition classes
+    //helps to prevent code duplication and keep step definitions CLEAN
+    @FindBy(id = "prependedInput")
+    public WebElement username;//will prevent u using this WebElement directly in step_definitions
+
+    @FindBy(id="prependedInput2")
+    private WebElement password;
+
+    @FindBy(name="_submit")
+    private WebElement submitButton;
+
+    public void login(String usernameValue, String passwordValue) {
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);//Keys.ENTER will be equal click on login button
+    }
+
+   //i am gonna overloading this login method, because i want to read Credential from configuration.properties file
+    public void login(){
+        String usernameValue = ConfigurationReader.getProperty("storemanager.username");
+        String passwordValue = ConfigurationReader.getProperty("password");
+
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+
+    public void login(String role ){
+
+        String usernameValue="";
+        String passwordValue = ConfigurationReader.getProperty("password");
+
+        if(role.equalsIgnoreCase("salesmanager")){
+             usernameValue = ConfigurationReader.getProperty("salesmanager.username");
+        }else if(role.equalsIgnoreCase("storemanager")){
+            usernameValue = ConfigurationReader.getProperty("storemanager.username");
+        }else{
+            usernameValue = ConfigurationReader.getProperty("driver.username");
+        }
+
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+
+
+  //  public void login(String...args){}//dynamic number of arguments
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,19 +1,19 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
 import com.vytrack.utils.ConfigurationReader;
 import com.vytrack.utils.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class LoginStepDefinitions {
+
+    LoginPage loginPage = new LoginPage();
    WebDriver driver;
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
@@ -28,10 +28,12 @@ public class LoginStepDefinitions {
 
     @When("user logs in")
     public void user_logs_in() throws InterruptedException {
-   //according to POM, elements are directly specified here
-     driver.findElement(By.id(("prependedInput"))).sendKeys(ConfigurationReader.getProperty("salesmanager110"));
-     driver.findElement(By.id("prependedInput2")).sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
-     Thread.sleep(3000);
+     //according to POM, elements are directly specified here
+//     driver.findElement(By.id(("prependedInput"))).sendKeys(ConfigurationReader.getProperty("salesmanager110"));
+//     driver.findElement(By.id("prependedInput2")).sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+
+
+        Thread.sleep(3000);
     }
     @Then("user should see dashboard page")
     public void user_should_see_dashboard_page() {
