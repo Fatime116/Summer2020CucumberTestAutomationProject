e2ab75513092d4056f1fe89f798fc5a0932dd6c1
Fatime116 <patigul116@gmail.com>
2021-01-14 22:04:35 -0800

    Base page methods

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -1,13 +1,29 @@
 package com.vytrack.pages;
 
 import com.vytrack.utils.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class BasePage {
+public abstract class BasePage {//it is abstract class, no one can create object from this class, it is a base class, it is not page class
+    //it does not correspond to any page in the application
+    //this class stores some common element and methods
+    //child class inherit everything from parent class except it is private and static
 
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(className="oro-subtitle")
+    protected WebElement pageSubTitle;//we can make it visible to the child classes(page class), still cannot have this webElement direct get access to step_definitions class
 
+
+   //we control access to WebElement through the private or protected keyword
+    //private encapsulate data inside the class, no one can get access to it
+    //protected: makes it available within the same package, and subclasses that inherits this class
+    //step definitions class are located in different package, and not going to inherit this class,so step_definition does not have any access to it
+
+    public String getPageSubTitleText(){ //this is instance method, in can inherit to child class
+        return pageSubTitle.getText().trim();
+    }
 }
--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -14,34 +14,42 @@ import org.openqa.selenium.WebDriver;
 public class LoginStepDefinitions {
 
     LoginPage loginPage = new LoginPage();
-   WebDriver driver;
+    WebDriver driver;
+
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
         //according to POM, driver cannot be instantiated here
-//        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.chromedriver().setup();  //makes the setup of webdriver, it is setting up chromedriver and connects the WebDriver object with the browser
 //        driver = new ChromeDriver();
 //        driver.get(ConfigurationReader.getProperty("url"));
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
     }
 
     @When("user logs in")
     public void user_logs_in() throws InterruptedException {
-     //according to POM, elements are directly specified here
+     //according to POM, elements are directly specified here,
+
 //     driver.findElement(By.id(("prependedInput"))).sendKeys(ConfigurationReader.getProperty("salesmanager110"));
 //     driver.findElement(By.id("prependedInput2")).sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
 
+        //since we are POM, we are creating page objects, literally instance of page class
+        //page object contains WebElements and methods,
 
-        Thread.sleep(3000);
+        loginPage.login();
+
     }
     @Then("user should see dashboard page")
     public void user_should_see_dashboard_page() {
         String expectedDashBoardName = "Dashboard";
+
+        //title class attribute value always the same and present for all tab header
         String actualDashBoardName = driver.findElement(By.cssSelector("h1[class='oro-subtitle']")).getText().trim();
         Assert.assertEquals("Title is not correct!!",expectedDashBoardName,actualDashBoardName);//in Junit comments comes before
 
-        driver.quit();
+//        driver.quit();
+        Driver.closeDriver();;
     }
 
 }
